8084ebbd3b0aedd73fd81ace2c70ea34467b13f2
Luisf66 <Luisf311220@gmail.com>
2025-02-21 14:13:07 -0300

    Cadastro de carros

--- carros/cars/forms.py
+++ carros/cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 class CarForm(forms.Form):
     model = forms.CharField(max_length=100)
@@ -9,3 +9,16 @@ class CarForm(forms.Form):
     value = forms.FloatField()
     plate = forms.CharField(max_length=10)
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_yaer = self.cleaned_data['model_yaer'],
+            value = self.cleaned_data['value'],
+            plate = self.cleaned_data['plate'],
+            photo = self.cleaned_data['photo']
+        )
+        car.save()
+        return car
