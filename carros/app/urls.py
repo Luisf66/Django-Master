4b9a9b4bf4cda63390204f5b58056378e6bf6a2a
Luisf66 <Luisf311220@gmail.com>
2025-02-26 11:05:06 -0300

    tela de update cars

--- carros/app/urls.py
+++ carros/app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import  CarsListView, NewCarCreateView, CarDetail
+from cars.views import  CarsListView, NewCarCreateView, CarDetail, CarUpdateView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -29,4 +29,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('car/<int:pk>/', CarDetail.as_view(), name='car_detail'),
+    path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 # Create your views here.
 
 class CarsListView(ListView):
@@ -25,3 +25,9 @@ class NewCarCreateView(CreateView):
 class CarDetail(DetailView):
     model = Car
     template_name = 'car_detail.html'
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
