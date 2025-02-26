8d220ec30b8b94f44f71def6abf6f63dc341685c
Luisf66 <Luisf311220@gmail.com>
2025-02-26 15:19:11 -0300

    tela de deletar carro

--- carros/app/urls.py
+++ carros/app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import  CarsListView, NewCarCreateView, CarDetail, CarUpdateView
+from cars.views import  CarsListView, NewCarCreateView, CarDetail, CarUpdateView, CarDeleteView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -30,4 +30,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('car/<int:pk>/', CarDetail.as_view(), name='car_detail'),
     path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
+    path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
