dd72abf530e000f5022a28bdb1479e170912588f
Luisf66 <Luisf311220@gmail.com>
2025-02-28 13:12:45 -0300

    campo bio no model car

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -19,6 +19,7 @@ class Car(models.Model):
     value = models.FloatField()
     plate = models.TextField(max_length=10, null=True, blank=True)
     photo = models.ImageField(upload_to='cars/', null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
